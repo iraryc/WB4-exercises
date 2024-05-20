f59c3da02a4cc8bf9c8c11e517223b08b7b5f731
Andrea Ceballos <iraryc@gmail.com>
2024-05-20 09:06:29 -0700

    practice
    practice

---
+++ ArrayScripts/football.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+window.onload = function() {
+let teams = [
+    {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
+    {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
+    {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
+    {code:"KAN", name:"Kansas City Chiefs", 
+    plays:"Kansas City, MO"},
+   ];
+
+
+
+
+
+
+}
